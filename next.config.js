3a87460b8b9839bd1b101d8f3ea00e87d5b42e8c
Farhad Omid <farhadino@gmail.com>
2024-10-21 22:53:40 +0200

    fix

--- next.config.js
+++ next.config.js
@@ -3,7 +3,6 @@ const path = require('path');
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
   webpack: (config, { isServer }) => {
     config.resolve.alias['tailwindcss'] = path.resolve(__dirname, 'node_modules/tailwindcss');
     return config;
