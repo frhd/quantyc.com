4d1ab8e061e0344a0af443418d797c98eb193d0a
Farhad Omid <farhadino@gmail.com>
2024-10-21 14:37:57 +0200

    u

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,6 @@
 import './global.css'
 import type { Metadata } from 'next'
-import { GeistSans } from 'geist/font/sans'
-import { GeistMono } from 'geist/font/mono'
-import { Navbar } from './components/nav'
+import { Inter } from 'next/font/google'
 import { Analytics } from '@vercel/analytics/react'
 import { SpeedInsights } from '@vercel/speed-insights/next'
 import Footer from './components/footer'
@@ -47,14 +45,11 @@ export default function RootLayout({
     <html
       lang="en"
       className={cx(
-        'text-black bg-white dark:text-white dark:bg-black',
-        GeistSans.variable,
-        GeistMono.variable
+        'text-black bg-white light:text-white dark:bg-white'
       )}
     >
       <body className="antialiased max-w-xl mx-4 mt-8 lg:mx-auto">
         <main className="flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0">
-          <Navbar />
           {children}
           <Footer />
           <Analytics />
--- app/page.tsx
+++ app/page.tsx
@@ -2,20 +2,31 @@ import { BlogPosts } from 'app/components/posts'
 
 export default function Page() {
   return (
-    <section>
+    <section className="py-[5rem] max-w-[370px]">
       <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
-        My Portfolio
+        <a href="https://quantyc.com" className="hover:underline">
+          🟥
+        </a>
       </h1>
       <p className="mb-4">
-        {`I'm a Vim enthusiast and tab advocate, finding unmatched efficiency in
-        Vim's keystroke commands and tabs' flexibility for personal viewing
-        preferences. This extends to my support for static typing, where its
-        early error detection ensures cleaner code, and my preference for dark
-        mode, which eases long coding sessions by reducing eye strain.`}
+      Ideas are only the start — what matters is how they’re brought to life.
+  </p>
+      <p className="mb-4">
+      <strong>quantyc</strong> works with companies of all sizes to deliver solutions that are fast, flexible, and dependable.
       </p>
-      <div className="my-8">
-        <BlogPosts />
-      </div>
+      <p className="mb-4">
+      From scalable web applications to complex enterprise systems, we bring the most challenging technical projects to life using technologies like Kubernetes, Spring Boot, and modern data and object storage systems.
+      </p>
+      <p className="mb-4">
+      Projects typically take 3–12 months. <br />
+      Start at €7,000/month for full-stack development.
+      </p>
+      <p className="mb-4">
+      **Explore our expertise** and see how we can elevate your digital capabilities.
+      </p>
+      <p className="mb-4">
+      **Get started with quantyc** or **book a call**.
+      </p>
     </section>
   )
 }
