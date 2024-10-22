a284c68e4a729ffbe4949079aa339ec47a4be621
Farhad Omid <farhadino@gmail.com>
2024-10-22 23:30:29 +0200

    poetic

--- app/page.tsx
+++ app/page.tsx
@@ -2,28 +2,27 @@ import { GeistMono } from 'geist/font/mono';
 
 export default function Page() {
   return (
-    <section className={`max-w-[420px] ${GeistMono.className} py-[2rem] md:py-[3rem]  transition-all duration-300 ease-in-out`}>
+    <section className={`max-w-[400px] ${GeistMono.className} py-[2rem] md:py-[3rem]  transition-all duration-300 ease-in-out`}>
       <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
         <a href="/" className="hover:underline">🟥</a>
       </h1>
       <p className="mb-4">
-        Ideas are only the start — what matters is how they're brought to life.
+        <strong>Ideas are just the beginning — <br /> execution is everything.</strong>
       </p>
       <p className="mb-4">
-        <strong>quantyc</strong> works with companies of all sizes to deliver solutions that are fast, flexible, and dependable.
+        At <strong>quantyc</strong>, we partner with companies of all sizes to deliver fast, flexible, and reliable solutions.
       </p>
       <p className="mb-4">
-        From <strong>scalable web applications</strong> to complex enterprise systems, we bring the most challenging technical projects to life using technologies like Kubernetes, Spring Boot, and modern data and object storage systems.
+        From scalable web applications to complex enterprise systems, we tackle the most demanding technical challenges, using cutting-edge technologies like Kubernetes, Spring Boot, and modern data and object storage solutions.
       </p>
       <p className="mb-4">
-        Projects typically take 3–18 months. <br />
-        Start at €7,000/month for full-stack development.
+        Our <strong><a href="/projects" className="text-red-600 hover:underline">projects</a></strong> typically span 3 to 18 months and start at €7,000/month for full-stack development services.
       </p>
       <p className="mb-4">
-        <a href="/projects" className="text-red-600 hover:underline">Explore our expertise</a> and see how we can take you to the next level.
+        <strong>Let’s take your business to the next level.</strong>
       </p>
       <p className="mb-4">
-        <a href="https://cal.com/quantyc" className="text-red-600 hover:underline" target="_blank" rel="noopener noreferrer">Book a call</a> to get started with us.
+        <strong><a href="https://cal.com/quantyc" className="text-red-600 hover:underline" target="_blank" rel="noopener noreferrer">Book a call</a></strong> today to get started.
       </p>
     </section>
   )
