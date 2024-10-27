9bab3d8ec629fd80b33edb6c2af3e9228c5de322
Farhad Omid <farhadino@gmail.com>
2024-10-27 10:10:14 +0100

    clean up

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   reactStrictMode: true,
   webpack: (config, { isServer }) => {
     config.resolve.alias['tailwindcss'] = path.resolve(__dirname, 'node_modules/tailwindcss');
