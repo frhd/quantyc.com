1998e46d3a9bd35b27bb3363d92c27721221615a
Farhad Omid <farhadino@gmail.com>
2024-10-27 10:12:41 +0100

    remove static export for now

--- next.config.js
+++ next.config.js
@@ -2,7 +2,6 @@ const path = require('path');
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
   reactStrictMode: true,
   webpack: (config, { isServer }) => {
     config.resolve.alias['tailwindcss'] = path.resolve(__dirname, 'node_modules/tailwindcss');
